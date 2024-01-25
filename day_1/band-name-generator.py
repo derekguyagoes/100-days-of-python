71fa53a715a422c0d8b5271a69e3e01e4da12230
derek <derek.guyagoes@gmail.com>
2024-01-25 16:27:37 -0700

    End of day 1

---
+++ day_1/band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What is the name of the city you grew up in?\n").capitalize()
+print(city)
+pet = input("What is your pets name?\n").capitalize()
+print(pet)
+print("Your band name could be " + city + " " + pet)
