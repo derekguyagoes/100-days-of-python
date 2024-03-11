6e582d95bab730cdbb8e10ae4080bf27cbb1fd06
derek <derek.guyagoes@gmail.com>
2024-03-11 17:15:04 -0600

    End of day 8

---
+++ day_8/caeser-cipher.py
@@ -0,0 +1,37 @@
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = (position + shift_amount) % len(alphabet)
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+print(logo)
+
+play_again = True
+while play_again:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    play_again_x = input("Do you want to play again? yes/no\n")
+    if play_again_x == "no":
+        play_again = False
+        print("goodbye")
