3eb376c863a928badcb9f1ed025e39f12f4e1cf2
Derek Gallegos <Derek.Gallegos@niceincontact.com>
2024-03-18 15:54:53 -0600

    End of day 12

--- day_12/main.py
+++ day_12/main.py
@@ -3,35 +3,47 @@ import random
 
 from day_12.art import logo
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
 print(logo)
 print("Welcome to the number guessing game!\nI'm thinking of a number between 1 and 100")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
-remaining_guesses = 0
 
-if difficulty == "easy":
-    remaining_guesses = 10
-else:
-    remaining_guesses = 5
-
-da_number = random.randint(1, 100)
-
-still_guessing = True
-while still_guessing:
-
-    print(f"You have {remaining_guesses} attempts remaning to guess the number")
-    guess = ""
-    guess = int(input("Make a guess: "))
-
-    if remaining_guesses > 1:
-        if guess > da_number:
-            print("Too high")
-        elif guess < da_number:
-            print("Too low")
-        elif guess == da_number:
-            print("you did it!")
-            still_guessing = False
-        remaining_guesses = remaining_guesses - 1
+def set_difficulty():
+    if difficulty == "easy":
+        return EASY_LEVEL_TURNS
     else:
-        still_guessing = False
-        print(f"The number was {da_number}")
+        return HARD_LEVEL_TURNS
+
+
+def get_random_number():
+    return random.randint(1, 100)
+
+
+def game():
+    answer = get_random_number()
+    remaining_guesses = set_difficulty()
+
+    still_guessing = True
+    while still_guessing:
+
+        print(f"You have {remaining_guesses} attempts remaning to guess the number")
+        guess = int(input("Make a guess: "))
+
+        if remaining_guesses > 1:
+            if guess > answer:
+                print("Too high")
+            elif guess < answer:
+                print("Too low")
+            elif guess == answer:
+                print("you did it!")
+                still_guessing = False
+            remaining_guesses = remaining_guesses - 1
+        else:
+            still_guessing = False
+            print(f"The number was {answer}")
+
+
+game()
