eaa3f96a2c958ed610459c56c727f291e342d92f
Derek Gallegos <Derek.Gallegos@niceincontact.com>
2024-03-22 13:29:04 -0600

    Casing and error handling.

--- day_14/main.py
+++ day_14/main.py
@@ -25,20 +25,24 @@ while keep_playing:
     print(f"Compare A: {champion["name"]} a {champion["description"]} from {champion["country"]}")
     print(vs)
     print(f"Against B: {challenger["name"]} a {challenger["description"]} from {challenger["country"]}")
-    guess = input("Who has more followers? Choose 'A' or 'B': ")
+    guess = input("Who has more followers? Choose 'A' or 'B': ").upper()
 
     if guess == 'A':
         if champion["follower_count"] > challenger["follower_count"]:
             score += 1
             print(f"correct a > b current score: {score}")
         else:
-            print("wrong b < a")
             keep_playing = False
-    else:
+            print(f"wrong b < a final score: {score}")
+
+    elif guess == 'B':
         if challenger["follower_count"] > champion["follower_count"]:
             print(f"correct b>a current score: {score}")
             score += 1
             champion = challenger
         else:
-            print("wrong b < a")
             keep_playing = False
+            print(f"wrong b < a final score: {score}")
+    else:
+        print("bad input, exiting")
+        keep_playing = False
