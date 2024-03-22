958f5f92122bfc06a9d65ab0f1d4dfe87f7f9f0d
Derek Gallegos <Derek.Gallegos@niceincontact.com>
2024-03-22 13:25:46 -0600

    Inital work completed of day 14

--- day_14/main.py
+++ day_14/main.py
@@ -14,21 +14,31 @@ def get_a_thing(first):
     return candidate
 
 
+keep_playing = True
+score = 0
 champion = random.choice(data)
-challenger = get_a_thing(champion)
 
-print(f"Compare A: {champion["name"]} a {champion["description"]} from {champion["country"]}")
-print(vs)
-print(f"Against B: {challenger["name"]} a {challenger["description"]} from {challenger["country"]}")
-guess = input("Who has more followers? Choose 'A' or 'B': ")
+while keep_playing:
 
-if guess == 'A':
-    if champion["follower_count"] > challenger["follower_count"]:
-        print("correct a > b")
+    challenger = get_a_thing(champion)
+
+    print(f"Compare A: {champion["name"]} a {champion["description"]} from {champion["country"]}")
+    print(vs)
+    print(f"Against B: {challenger["name"]} a {challenger["description"]} from {challenger["country"]}")
+    guess = input("Who has more followers? Choose 'A' or 'B': ")
+
+    if guess == 'A':
+        if champion["follower_count"] > challenger["follower_count"]:
+            score += 1
+            print(f"correct a > b current score: {score}")
+        else:
+            print("wrong b < a")
+            keep_playing = False
     else:
-        print("wrong b < a")
-else:
-    if challenger["follower_count"] > champion["follower_count"]:
-        print("correct b>a")
-    else:
-        print("wrong b < a")
+        if challenger["follower_count"] > champion["follower_count"]:
+            print(f"correct b>a current score: {score}")
+            score += 1
+            champion = challenger
+        else:
+            print("wrong b < a")
+            keep_playing = False
