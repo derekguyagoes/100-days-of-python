5d00103161a6a807f572f1deb9c00a451424a53d
derek <derek.guyagoes@gmail.com>
2024-06-13 16:14:39 -0600

    End of day 22

--- day_22/app.py
+++ day_22/app.py
@@ -3,6 +3,7 @@ from turtle import Screen, Turtle
 
 from day_22.Paddle import Paddle
 from ball import Ball
+from day_22.scoreboard import Scoreboard
 
 # pong
 # screate teh screen divider in the middle
@@ -23,7 +24,7 @@ divider = Turtle()
 # divider.penup()
 divider.goto(0, -780)
 divider.color("white")
-divider.goto(0,800)
+divider.goto(0, 800)
 
 rightPaddle = Paddle(350)
 leftPaddle = Paddle(-350)
@@ -36,10 +37,29 @@ screen.onkey(leftPaddle.go_down, "s")
 
 ball = Ball()
 
+scoreboard = Scoreboard()
+
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    #detect collision with right paddle
+    if (ball.distance(rightPaddle) < 50 and ball.xcor() > 320 or
+            ball.distance(leftPaddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    #detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
