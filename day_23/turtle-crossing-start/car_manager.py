00842172194f90584246df4fdfc5f7ab5e7e4cc6
derek <derek.guyagoes@gmail.com>
2024-06-17 16:55:55 -0600

    Day 23, adding cars, movable turtle.

--- day_23/turtle-crossing-start/car_manager.py
+++ day_23/turtle-crossing-start/car_manager.py
@@ -1,7 +1,27 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+BOTTOM = -500
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.cars = []
+
+        for _ in range(10):
+            new_car = Car()
+            self.cars.append(new_car)
+
+
+
+class Car(Turtle):
+    def __init__(self):
+        super().__init__()
+
+        self.shape("square")
+        self.color(random.choice(COLORS))
+        self.penup()
+        self.goto(500, BOTTOM)
