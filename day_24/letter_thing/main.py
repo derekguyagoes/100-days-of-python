154b83a5f3544cde00321cab12222ea167ed1977
derek <derek.guyagoes@gmail.com>
2024-07-18 16:38:30 -0600

    Day 24 - moving for out of with.

--- day_24/letter_thing/main.py
+++ day_24/letter_thing/main.py
@@ -2,8 +2,7 @@ import os
 
 import helpers
 
-helpers.delete_garb_files(f"{os.getcwd()}/Output/garb")
-
+helpers.delete_garb_files(f"{os.getcwd()}/Output/ReadyToSend")
 
 with open("./Input/Letters/starting_letter.txt") as file:
     letter = file.readlines()
@@ -11,11 +10,11 @@ with open("./Input/Letters/starting_letter.txt") as file:
 with open("./Input/Names/invited_names.txt") as names_file:
     r = (names_file.readlines())
 
-    for n in r:
-        name = n.strip()
-        with open(f"./Output/ReadyToSend/{name}", "w") as output_file:
-            for line in letter:
-                if "[name]" in line:
-                    output_file.write(line.replace("[name]", name))
-                else:
-                    output_file.write(line)
+for n in r:
+    name = n.strip()
+    with open(f"./Output/ReadyToSend/{name}", "w") as output_file:
+        for line in letter:
+            if "[name]" in line:
+                output_file.write(line.replace("[name]", name))
+            else:
+                output_file.write(line)
