7b33eb95b0fe08d49f6af7bb058b7d8a5bd1e6fc
derek <derek.guyagoes@gmail.com>
2024-07-18 16:42:18 -0600

    Day 24 - end

--- day_24/letter_thing/main.py
+++ day_24/letter_thing/main.py
@@ -8,9 +8,9 @@ with open("./Input/Letters/starting_letter.txt") as file:
     letter = file.readlines()
 
 with open("./Input/Names/invited_names.txt") as names_file:
-    r = (names_file.readlines())
+    names = (names_file.readlines())
 
-for n in r:
+for n in names:
     name = n.strip()
     with open(f"./Output/ReadyToSend/{name}", "w") as output_file:
         for line in letter:
