a22d6dbb9d6ef9e618e6d8e449c1cd881e2d1bfe
derek <derek.guyagoes@gmail.com>
2024-07-29 19:13:44 -0600

    Day 25 end

--- day_25/day-25-us-states-game-start/main.py
+++ day_25/day-25-us-states-game-start/main.py
@@ -1,5 +1,6 @@
 import turtle
 
+import pandas
 import pandas as pd
 
 screen = turtle.Screen()
@@ -13,26 +14,33 @@ turtle.penup()
 
 data = pd.read_csv("50_states.csv")
 all_states = data["state"].to_list()
-correct_states = []
+guessed_states = []
 
-while len(correct_states) < 50:
+while len(guessed_states) < 50:
     t = turtle.Turtle()
     t.hideturtle()
     t.penup()
     t.goto(0, 0)
     answer_state = screen.textinput(
-        title=f"{len(correct_states)}/50 States Correct",
+        title=f"{len(guessed_states)}/50 States Correct",
         prompt="Which state do you want to play?",
     ).title()
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
-        if answer_state in correct_states:
+        if answer_state in guessed_states:
             print("you've already guessed that")
         else:
             state_data = data[data.state == answer_state]
-            correct_states.append(answer_state)
+            guessed_states.append(answer_state)
             t.goto(state_data.x.item(), state_data.y.item())
 
             t.write(answer_state)
-
-turtle.mainloop()
