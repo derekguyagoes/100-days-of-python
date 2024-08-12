f2837dd884d455da7c25e9b83c1753b1242af352
derek <derek.guyagoes@gmail.com>
2024-08-12 15:27:53 -0600

    Day 27 end

--- day_27/miles_to_km.py
+++ day_27/miles_to_km.py
@@ -1,22 +1,23 @@
 from tkinter import *
 
-KM_VALUE = 1.609344
+KM_VALUE = 1.609
 
 
-def calculate():
-    miles = int(input.get())
-    calculated_value.config(text=miles * KM_VALUE)
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = miles * KM_VALUE
+    calculated_value.config(text=f"{km}")
 
 
 window = Tk()
 window.title("Miles to KM Converter")
-window.minsize(width=500, height=250)
-window.config(padx=10, pady=10)
+window.config(padx=20, pady=20)
 
 # row 0
 # null, textbox label
-input = Entry(width=5)
-input.grid(row=0, column=1)
+miles_input = Entry(width=5)
+miles_input.grid(row=0, column=1)
+
 input_label = Label(text="Miles:")
 input_label.grid(row=0, column=2)
 
@@ -33,6 +34,7 @@ km.grid(row=1, column=2)
 
 # row 2
 # null, button, null
-calculate = Button(text="Calculate", command=calculate)
+calculate = Button(text="Calculate", command=miles_to_km)
 calculate.grid(row=2, column=1)
+
 window.mainloop()
