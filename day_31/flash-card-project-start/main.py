25a7bf647e098a6bb2b8117ce7cdd11a84727589
derek <derek.guyagoes@gmail.com>
2024-08-20 18:32:46 -0600

    Day 31 final ui

--- day_31/flash-card-project-start/main.py
+++ day_31/flash-card-project-start/main.py
@@ -5,27 +5,24 @@ TITLE_FONT = ("Arial", 40, "italic")
 WORD_FONT = ("Arial", 60, "bold")
 
 window = Tk()
-window.title("Flash Card Project")
-window.config(padx=50, pady=50)
+window.title("Flash")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=800, bg=BACKGROUND_COLOR)
+canvas = Canvas(width=800, height=526)
 card_front = PhotoImage(file="./images/card_front.png")
-card_back = PhotoImage(file="./images/card_back.png")
-canvas.create_image(526, 800, image=card_front)
+canvas.create_image(400, 263, image=card_front)
+canvas.create_text(400, 150, text="title", font=TITLE_FONT, fill="black")
+canvas.create_text(400, 263, text="word", font=WORD_FONT, fill="black")
+canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-title_label = Label(text="title", font=TITLE_FONT)
-title_label.grid(row=0, column=0)
-
-word_label = Label(text="word", font=WORD_FONT)
-word_label.grid(row=0, column=1)
-
+card_back = PhotoImage(file="./images/card_back.png")
 pass_image = PhotoImage(file="./images/wrong.png")
-pass_button = Button(image=pass_image)
-pass_button.grid(row=1, column=0)
+unknown_button = Button(image=pass_image, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image)
+right_button = Button(image=right_image, highlightthickness=0)
 right_button.grid(row=1, column=1)
 
 window.mainloop()
