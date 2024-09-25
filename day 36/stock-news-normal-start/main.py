79bb13b9f7ca5d84a9aba15fb11343de4a3ee39a
derek <derek.guyagoes@gmail.com>
2024-09-25 17:00:26 -0600

    Day 36 getting data and using default data when at rate limit. Adding config files to gitignore.

--- day 36/stock-news-normal-start/main.py
+++ day 36/stock-news-normal-start/main.py
@@ -1,40 +1,78 @@
+import json
+
+import requests
+import datetime as dt
+import configparser
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
-
-#TODO 2. - Get the day before yesterday's closing stock price
-
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+config = configparser.ConfigParser()
+config.read("config.ini")
+key = config["DEFAULT"]["key"]
 
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
+def get_stock_difference_percent():
+    url = (
+        f"{STOCK_ENDPOINT}?function=TIME_SERIES_DAILY&symbol={STOCK_NAME}&apikey={key}"
+    )
+    r = requests.get(url)
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+    if "Thank you for using Alpha Vantage!" not in r.text:
+        data = r.json()
+        yesterday = (dt.date.today() - dt.timedelta(days=1)).strftime("%Y-%m-%d")
+        day_before_yesterdays = (dt.date.today() - dt.timedelta(days=2)).strftime(
+            "%Y-%m-%d"
+        )
+    else:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+        yesterday = "2024-09-25"
+        day_before_yesterdays = "2024-09-24"
 
-#TODO 9. - Send each article as a separate message via Twilio. 
+    yesterdays_closing = eval(data["Time Series (Daily)"][yesterday]["4. close"])
+    day_before_yesterdays_closing = eval(
+        data["Time Series (Daily)"][day_before_yesterdays]["4. close"]
+    )
+    percent = round(
+        (
+                abs(yesterdays_closing - day_before_yesterdays_closing)
+                / day_before_yesterdays_closing
+        )
+        * 100.0
+    )
+
+    return percent
 
 
+def get_news():
+    return "get news"
 
-#Optional TODO: Format the message like this: 
+
+percent = get_stock_difference_percent()
+if percent > 5:
+    print(get_news())
+
+## STEP 2: https://newsapi.org/
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+# TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+
+# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+
+
+## STEP 3: Use twilio.com/docs/sms/quickstart/python
+# to send a separate message with each article's title and description to your phone number.
+
+# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+
+# TODO 9. - Send each article as a separate message via Twilio.
+
+
+# Optional TODO: Format the message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
@@ -44,4 +82,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
